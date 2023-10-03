6a346e9569180d160ceeee4eeacf8b02186088ec
rubel04 <alomrana886@gmail.com>
2023-10-03 20:48:01 +0600

    added login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../SharedPages/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className="text-2xl">This is a login page</h2>
+        </div>
+    );
+};
+
+export default Login;
--- src/Pages/SharedPages/Navbar/Navbar.jsx
+++ src/Pages/SharedPages/Navbar/Navbar.jsx
@@ -13,6 +13,9 @@ const Navbar = () => {
       <li>
         <NavLink to="/career">Career</NavLink>
       </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
     </>
   );
   return (
